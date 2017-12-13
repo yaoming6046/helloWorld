41de2f9d733a76753e319095a30f1ab31d988659
yaoming <yaoming@hualala.com>
2017-12-13 23:04:43 +0800

    init

---
+++ src/test/java/org/fortunate179/helloWorld/TestBase.java
@@ -0,0 +1,11 @@
+package org.fortunate179.helloWorld;
+
+import org.junit.Test;
+
+public class TestBase {
+
+    @Test
+    public void testAbc(){
+        System.out.println("hello world");
+    }
+}
